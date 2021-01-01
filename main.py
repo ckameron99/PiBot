b06c82328d31e716ef36cc327f8d0921a74e44eb
ckameron99 <ckameron99@gmail.com>
2021-01-01 13:46:26 -0800

    added filler code to test connection

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 import api
+import time
 class robot():
     def __init__(self):
         self.api=api(self)
 
     def powerOptions(self,option):
         pass
+r=robot()
+while True:
+    time.sleep(10)
