49880735821430a3446d1e60c28019ee4f39ebd6
ckameron99 <ckameron99@gmail.com>
2021-01-01 14:06:05 -0800

    bug fixes

--- api.py
+++ api.py
@@ -19,7 +19,7 @@ class API():
 
     def listen(self, name):
         s=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        s.bind((self.appIP, self.listenPort))
+        s.bind((self.robotIP, self.listenPort))
         s.listen()
         while True:
             data=''
